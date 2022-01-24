7759a8c4908484518f21e5986f91c09d4477254e
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-24 18:41:01 +0300

    Update to scripts file

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 'use strict';
-const menubtn = document.getElementById('menu');//parent for
-const mobilemenu = document.querySelector('.menu-links');//links
-const menu = document.querySelector('.menu-icon');//img
+const menubtn = document.getElementById('menu');
+const mobilemenu = document.querySelector('.menu-links');
+const menu = document.querySelector('.menu-icon');
 
+mobilemenu.style.display = 'none';
+menubtn.onclick = function () {
+if (mobilemenu.style.display === 'none') {
+mobilemenu.style.display = 'block';
+menu.src = 'images/close.png';
+} else {
+mobilemenu.style.display = 'none';
+menu.src = 'images/Union.svg';
+}
+};
