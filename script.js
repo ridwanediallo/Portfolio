e5fb45ca0df11488e702a56e65838575753ef0e5
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-24 18:18:01 +0300

    Update to scripts variable declarations

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+const menubtn = document.getElementById('menu');//parent for
+const mobilemenu = document.querySelector('.menu-links');//links
+const menu = document.querySelector('.menu-icon');//img
+
