3c2c17b43c0aaa84df085c59d43d82e18ffa35a8
Ridwane <ridwanediallo99@gmail.com>
2022-01-24 21:27:09 +0000

    Link the links to the different sections and update the width of the menu-links

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-'use strict';
-const menubtn = document.getElementById('menu');
-const mobilemenu = document.querySelector('.menu-links');
-const menu = document.querySelector('.menu-icon');
+"use strict";
+const menubtn = document.getElementById("menu");
+const mobilemenu = document.querySelector(".menu-links");
+const menu = document.querySelector(".menu-icon");
 
-mobilemenu.style.display = 'none';
+mobilemenu.style.display = "none";
 menubtn.onclick = function () {
-if (mobilemenu.style.display === 'none') {
-mobilemenu.style.display = 'block';
-menu.src = 'images/close.png';
-} else {
-mobilemenu.style.display = 'none';
-menu.src = 'images/Union.svg';
-}
+  if (mobilemenu.style.display === "none") {
+    mobilemenu.style.display = "block";
+    menu.src = "images/close.png";
+  } else {
+    mobilemenu.style.display = "none";
+    menu.src = "images/Union.svg";
+  }
 };
