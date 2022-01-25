dc58deab8bcd68312e33d5cd203b4727d0c78231
Ridwane <ridwanediallo99@gmail.com>
2022-01-25 08:52:45 +0000

    Loop over the mobile menu links to hide the menu

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const menubtn = document.getElementById("menu");
 const mobilemenu = document.querySelector(".menu-links");
 const menu = document.querySelector(".menu-icon");
+const contactLink = document.querySelectorAll(".ctc-link a");
 
 mobilemenu.style.display = "none";
+
 menubtn.onclick = function () {
   if (mobilemenu.style.display === "none") {
     mobilemenu.style.display = "block";
@@ -13,3 +15,10 @@ menubtn.onclick = function () {
     menu.src = "images/Union.svg";
   }
 };
+
+contactLink.forEach((element) => {
+  element.addEventListener("click", function () {
+    mobilemenu.style.display = "none";
+    menu.src = "images/Union.png";
+  });
+});
