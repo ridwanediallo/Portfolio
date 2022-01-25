295b9555a70933c04291940f6321aa1de56415bd
Ridwane <ridwanediallo99@gmail.com>
2022-01-25 15:04:26 +0000

    fix linters errors

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-"use strict";
 const menubtn = document.querySelector(".open");
 const closebtn = document.querySelector(".close");
 const logo = document.querySelector(".logo");
 const mobilemenu = document.querySelector(".menu-links");
-const menu = document.querySelector(".menu-icon");
 const contactLink = document.querySelectorAll(".ctc-link a");
 
-mobilemenu.style.display = "none";
+mobilemenu.style.display = 'none';
 
-menubtn.addEventListener("click", function () {
-  if (mobilemenu.style.display === "none") {
-    mobilemenu.style.display = "block";
-    logo.style.display = "none";
-    menubtn.style.display = "none";
-  } else {
-    mobilemenu.style.display = "none";
-    logo.style.display = "block";
-    menubtn.style.display = "block";
+menubtn.addEventListener('click', () => {
+  if (mobilemenu.style.display === 'none') {
+    mobilemenu.style.display = 'block';
+    logo.style.display = 'none';
+    menubtn.style.display = 'none';
+  } 
+  else {
+    mobilemenu.style.display = 'none';
+    logo.style.display = 'block';
+    menubtn.style.display = 'block';
   }
 });
 
-closebtn.addEventListener("click", function () {
-  if (mobilemenu.style.display === "block") {
-    mobilemenu.style.display = "none";
-    logo.style.display = "block";
-    menubtn.style.display = "block";
+closebtn.addEventListener('click', () => {
+  if (mobilemenu.style.display === 'block') {
+    mobilemenu.style.display = 'none';
+    logo.style.display = 'block';
+    menubtn.style.display = 'block';
   }
 });
 
 contactLink.forEach((element) => {
-  element.addEventListener("click", function () {
+  element.addEventListener('click', () => {
     mobilemenu.style.display = "none";
-    logo.style.display = "block";
-    menubtn.style.display = "block";
+    logo.style.display = 'block';
+    menubtn.style.display = 'block';
   });
 });
