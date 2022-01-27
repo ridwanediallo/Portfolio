149397c12a6e7ea3f32d421dae0dfbab6dcc5b73
Ridwane <ridwanediallo99@gmail.com>
2022-01-27 13:18:29 +0000

    Add card function

--- modal.js
+++ modal.js
@@ -1,26 +1,26 @@
 const popUp = document.querySelector('.popup');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
-const seeProjectBtn = document.querySelector('.see-project');
+const seeProjectBtn = document.querySelectorAll('.see-project');
 const detailSection = document.querySelector('.detail-section');
 console.log(seeProjectBtn);
 
-seeProjectBtn.addEventListener('click', () => {
-  popUp.style.display = 'block';
-  modal.style.display = 'block';
-});
 
 const data = [
   {
-    id: '1',
-    name: 'Tonic',
-    image: 'images/Snapshoot1.png',
-    technologies: ['HTML', 'CSS', 'javascript'],
+    id: 'project-1',
+    title: 'Tonic',
+    canopy: 'Canopy',
+    html: 'html',
+    css: 'css',
+    javascript: 'javasctipt',
+    image1: 'images/Snapshoot1.png',
+    image2: 'images/desktop/Snapshoot-Portfolio.png',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?',
   },
   {
-    id: '2',
+    id: 'project-2',
     name: 'Multi-Post Stories',
     image: 'images/Snapshoot2.png',
     technologies: ['HTML', 'CSS', 'javascript'],
@@ -28,19 +28,102 @@ const data = [
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?',
   },
   {
-    id: '3',
-    name: 'Tonic',
+    id: 'project-3',
+    title: 'Tonic',
+    canopy: 'Canopy',
     image: 'images/Snapshoot3.png',
-    technologies: ['HTML', 'CSS', 'javascript'],
+    html: 'html',
+    css: 'css',
+    javascript: 'javasctipt',
+    image1: 'images/Snapshoot1.png',
+    image2: 'images/desktop/Snapshoot-Portfolio.png',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?',
   },
   {
-    id: '4',
+    id: 'project-4',
     name: 'Multi-Post Stories',
     image: 'images/Snapshoot4.png',
     technologies: ['HTML', 'CSS', 'javascript'],
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?',
   },
+  {
+    id: 'project-5',
+    name: 'Tonic',
+    image1: 'images/Snapshoot1.png',
+    image2: 'images/desktop/Snapshoot-Portfolio.png',
+    html: 'html',
+    bootstrap: 'bootstrap',
+    ruby: 'Ruby On Rails',
+    github: 'github',
+    css: 'css',
+    javascript: 'javasctipt',
+    description: `
+    Lorem Ipsum is simply dummy text of the printing and
+    typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it 1960s with the
+    releaLorem Ipsum is simply dummy text of the printing and
+    typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it 1960s with the
+    releorem Ipsum is simply dummy text of the printing and
+    typesetting industry.`,
+  },
 ];
+
+const card = data.map((elem) => {
+  const modalHtml = `
+<div class="popup">
+        <div class="modal">
+          <div class="modal-div-1">
+            <h2 class="modal-title">${elem.name}</h2>
+            <ion-icon class="close-modal" name="close-outline"></ion-icon>
+          </div>
+          <div class="modal-div-2">
+            <p class="canopy">${elem.canopy}</p>
+            <p class="back-end">
+              <img class="counter" src="images/Counter.png" alt="counter" />
+              Back End Dev
+            </p>
+            <p class="back-end">
+              <img class="counter" src="images/Counter.png" alt="counter" />
+              2015
+            </p>
+          </div>
+          <div class="modal-div-3">
+            <div class="modal-img">
+              <img class="snapshoot1-mob" src="images/Snapshoot1.png" alt="snapshoot1" />
+              <img class="snapshoot1-desk" src="images/desktop/Snapshoot-Portfolio.png" alt="snapshoot1" />
+            </div>
+            <div class="modal-mobile-detail">
+              <!-- <div> -->
+                <p class="modal-description desk mob"></p>
+              <div class="tech">
+                <ul class="modal-tools mobil">
+                  <li class="tool">html</li>
+                  <li class="tool">css</li>
+                  <li class="tool">javaScript</li>
+                </ul>
+                <ul class="modal-tools desk">
+                  <li class="tool">github</li>
+                  <li class="tool">ruby</li>
+                  <li class="tool">Bootstrap</li>
+                </ul>
+                <button type="button" class="modal-btn">
+                  See Live <img src="images/Icon-see-live.png" alt="live" />
+                </button>
+                <button type="button" class="modal-btn">
+                  See Source
+                  <img src="images/Icon-see-source.png" alt="Github" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+ `;
+});
+
+const modalPopUp = () => {};
