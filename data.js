691ea592fb979603e2d20428b213bd1291677628
ridwan <ridwanediallo99@gmail.com>
2022-11-23 22:30:26 +0000

    Add data

---
+++ data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 'project-2',
+    title: `Space Traveler's Hub`,
+    image: 'images/space-traveler.png',
+    tech1: 'React',
+    tech2: 'Redux',
+    tech3: 'Bootstrap',
+    tech4: ' React-Router',
+    end: 'Front-End Dev',
+    description: `Space Travelers' hub is a group project app built by me and my partner Saningo it allows the user to reserve a rocket and joined a mission. The app fetches the data from an external API.`,
+  },
+  {
+    id: 'project-3',
+    title: 'Tonic',
+    canopy: 'Canopy',
+    image: 'images/countries.png',
+    tech1: 'React',
+    tech2: 'Redux',
+    tech3: 'React-Router',
+    // tech4: ' React-Router',
+    description:
+      'The entire world is a React, React-Router, Redux Toolkit, and Bootstrap app. It makes use of the RESTful Countries API to display information about countries such as their flag, population, timezone, and so on. It has two versions with different layouts.',
+  },
+  {
+    id: 'project-4',
+    title: 'BookStore CMS',
+    canopy: 'Canopy',
+    image: 'images/bookstore.png',
+    tech1: 'React',
+    // tech4: ' React-Router',
+    tech2: 'Redux',
+    tech3: 'React-Router',
+    end: 'Front-End Dev',
+    description: `This is an app for users to add a book's title, its author and category. Once added the book's data are saved to an external API and when the app reloads, it fetches the data from the server and displays them on the screen.`,
+  },
+];
+
+export default data;
